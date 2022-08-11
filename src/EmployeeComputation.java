e7b31101e390a906a7b1116ae974c9784885cff6
Pooja <pooja.katkr@gmail.com>
2022-08-11 15:16:58 +0530

    Uc2 added

--- src/EmployeeComputation.java
+++ src/EmployeeComputation.java
@@ -11,6 +11,19 @@ public class EmployeeComputation {
 			   System.out.println("Employee is Present");
 			else
 		       System.out.println("Employee is Abscent");		
+			
+			IS_FULL_TIME = 1;
+			int EMP_RATE_PER_HOUR = 20;
+			int empHrs = 0;
+			int empWage = 0;
+			empCheck = Math.floor(Math.random() * 10) %2;
+			if (empCheck == IS_FULL_TIME)
+				empHrs = 8;
+			else 
+				empHrs = 0;
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("emp Wage:" + empWage);
+			
 			}
 	}
 
