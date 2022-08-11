1c9901cf439d64737ecc2b94c2108178e3e3ab84
Pooja <pooja.katkr@gmail.com>
2022-08-11 15:20:14 +0530

    Uc3 added

--- src/EmployeeComputation.java
+++ src/EmployeeComputation.java
@@ -24,6 +24,22 @@ public class EmployeeComputation {
 			empWage = empHrs * EMP_RATE_PER_HOUR;
 			System.out.println("emp Wage:" + empWage);
 			
+			int IS_PART_TIME = 1;
+			IS_FULL_TIME = 2;
+			EMP_RATE_PER_HOUR = 20;
+			empHrs = 0;
+			empWage = 0;
+			empCheck = Math.floor(Math.random() * 10) % 3;
+			if (empCheck == IS_PART_TIME)
+				empHrs = 4;
+			else if (empCheck == IS_FULL_TIME)
+				empHrs = 8;
+			else
+				empHrs = 0;
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage:" + empWage);
+			
+			
 			}
 	}
 
